472f89e9fdd4890d0a43d1d7f1256f0a742103d8
Avaer Kazmer <a@modules.io>
2020-09-11 08:04:30 -0400

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,15 @@
-pushd /Users/a/Documents/GitHub/flow-js-sdk/examples/react-simple/
+pushd ./examples/react-simple/
+npm i
+popd
+pushd ./packages/sdk/
+npm i
+npm run build
+popd
+pushd ./packages/types/
+npm i
+npm run build
+popd
+pushd ./packages/dev-wallet/
 npm i
 popd
 parcel build --target=browser index.js
