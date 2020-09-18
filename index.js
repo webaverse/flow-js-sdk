abbcad0e89fd7a93d2f2cf7376df6b589cc87d7d
Avaer Kazmer <a@modules.io>
2020-09-18 07:14:39 -0400

    Bugfix index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import * as sdk from './packages/sdk/dist/sdk.module.js';
 import * as types from './packages/types/dist/types.module.js';
-import * as crypto from './packages/dev-wallet/server/crypto/dist/index.js';
+import * as crypto from './packages/dev-wallet/server/crypto/index.js';
 import * as signingFunction from './examples/react-simple/src/ix/utils/signing-function.js';
 export {
   sdk,
